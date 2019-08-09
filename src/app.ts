0e1f2462eac18ce1cea27e142bc3e9c09f8b182f
f2hard3 <f2hard3@gmail.com>
2019-08-10 00:46:37 +0900

    chore(eslint): update eslint rules

--- src/app.ts
+++ src/app.ts
@@ -8,7 +8,6 @@ const app = express();
 
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
-const list = 'sadasd';
 /**
  * app routes
  */
--- src/controllers/user.ts
+++ src/controllers/user.ts
@@ -1,5 +1,3 @@
 import { Request, Response, NextFunction } from 'express';
 
-export const login = (req: Request, res: Response, next: NextFunction) => {
-    const string = 'test';
-};
+export const login = (req: Request, res: Response, next: NextFunction) => {};
